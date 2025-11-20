32ef963014689df66371cdfd1550224dddee7e95
Muhammad-Okasha-Khan <149578718+Muhammad-Okasha-Khan@users.noreply.github.com>
2025-11-20 08:11:30 +0500

    Change model to 'rehub' and modify run settings
    Updated model type and adjusted script parameters.

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,6 +1,6 @@
 dataset="HCPGender"
 batch_size="16"
-model="GCNConv"
+model="rehub"
 hidden="64"
 main="main_new.py"
-python $main --dataset $dataset --model $model --device 'cuda' --batch_size $batch_size --runs 10
+python $main --hidden $hidden--dataset $dataset --model $model --device 'cuda' --batch_size $batch_size --runs 1
